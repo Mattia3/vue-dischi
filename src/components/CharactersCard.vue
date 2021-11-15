5082eee1a66e522165290dd322a4c643ae4bb970
Mattia3 <mcortinovis9@gmail.com>
2021-11-15 18:05:36 +0100

    Esercizio completato

---
+++ src/components/CharactersCard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+     <div class="background-card">
+        <img class="my-img" :src="poster" alt="...">
+        <div class="my-card-body">
+          <h3 class="text-center text-white text-uppercase">{{ title }}</h3>
+          <div class="subtitle-container">
+           <h5>{{ author }}</h5>
+           <h5>{{ year }}</h5>
+          </div>
+        </div>
+      </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CharactersCard",
+  props:{
+    poster: String,
+    title: String,
+    author: String,
+    year: String
+
+  },
+}
+</script>
+
+<style>
+  .background-card{
+    background-color: rgba(43,58,70,255);
+    display: flex;
+    flex-direction: column;
+    min-height: 450px;
+  }
+  .my-img{
+    padding: 20px;
+  }
+
+  .my-card-body{
+    padding: 0 11px;
+  }
+
+  .subtitle-container{
+    text-align: center;
+    padding: 10px 0px;
+  }
+
+
+
+</style>
---
+++ src/components/CharactersContainer.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <div class="background-header"></div>
+    <div class="container-background">
+      <div class="container">
+        <div class="row row-cols-md-5 g-4  py-5">
+          <div class="col" v-for="(character, i) in characterList" :key="i">
+            <CharactersCard 
+              :poster="character.poster"
+              :title="character.title"
+              :author="character.author"
+              :year="character.year"
+            ></CharactersCard>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import CharactersCard from "./CharactersCard.vue"
+
+export default {
+  name: "CharactersContainer",
+  components:{
+    CharactersCard,
+  },
+  data(){
+    return{
+      characterList: [],
+    }
+  },
+  mounted(){
+    axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+    .then(resp => {
+      this.characterList.push(...resp.data.response)
+     
+    })
+  }
+
+}
+
+</script>
+
+<style  lang="scss">
+  .background-header{
+  background-color: rgba(43,58,70,255);
+  height: 70px;
+  }
+
+  .container-background{
+    background-color: rgba(25,45,59,255);
+  }
+
+  
+
+</style>
